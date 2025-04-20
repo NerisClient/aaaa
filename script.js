b86665cffc8ba64db30ba68131cf5f9b1c2c70da
NerisClient <arturignatenko37@gmail.com>
2025-04-20 04:27:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function buyPremium() {
+    window.location.href = 'https://example.com/premium'; // Замени ссылку
+  }
+  
+  function downloadFree() {
+    window.location.href = 'https://example.com/free'; // Замени ссылку
+  }
+  
