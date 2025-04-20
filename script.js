a94a57a486e613262d796567c62e98a939416aaa
NerisClient <arturignatenko37@gmail.com>
2025-04-20 21:13:52 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Функции для кнопок
-function buyPremium() {
-    window.location.href = 'https://example.com/premium'; // Укажите правильную ссылку для покупки
-  }
-  
-  function downloadFree() {
-    window.location.href = 'https://example.com/free'; // Укажите правильную ссылку для скачивания
-  }
-  
