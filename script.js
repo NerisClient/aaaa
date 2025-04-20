82af39c18b5be9f0c154532fb49845cc4f925cca
NerisClient <arturignatenko37@gmail.com>
2025-04-20 15:53:31 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function buyPremium() {
-    window.location.href = 'https://example.com/premium'; // Замени ссылку
-  }
-  
-  function downloadFree() {
-    window.location.href = 'https://example.com/free'; // Замени ссылку
-  }
-  
